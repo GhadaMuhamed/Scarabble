b7f135443ce1c17a921fd4eba22a4ef7df7eab09
Berlnty <berlnty456@gmail.com>
2018-12-15 14:39:33 +0200

    Update LearnedWeights.h

--- LearnedWeights/LearnedWeights.h
+++ LearnedWeights/LearnedWeights.h
@@ -5,14 +5,14 @@ using namespace std;
 
 #include "Move.h"
 #include "Board.h"
-#include "Judge.h"
+#include "Ju3..dge.h"
 
 class learnedWeights
 {
 private:
 
-	int opponent[4] = {0,0,0,0}; // oppnent's bingo,perm,rack, total score
-	int agent[4] = {0,0,0,0};    //my bingo,perm,rack, total score
+	float opponent[4] = {0,0,1,0}; // oppnent's bingo,perm,rack, total score
+	float agent[4] = {0,0,1,0};    //my bingo,perm,rack, total score
 	int opp_moves,my_moves; // the number of moves of each player
 	double Statistics[4];  // bingo,permium,rack,total # of steps
 	double W[3];  //the learned weights
